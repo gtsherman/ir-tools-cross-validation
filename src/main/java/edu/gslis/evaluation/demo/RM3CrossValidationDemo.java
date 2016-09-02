4a5f9442e71bf0ab9910ae8fde2397f0d1c6790c
Garrick Sherman <gsherma2@illinois.edu>
2016-09-02 17:16:34 -0500

    use new utils lib

--- src/main/java/edu/gslis/evaluation/demo/RM3CrossValidationDemo.java
+++ src/main/java/edu/gslis/evaluation/demo/RM3CrossValidationDemo.java
@@ -15,9 +15,9 @@ import edu.gslis.output.FormattedOutputTrecEval;
 import edu.gslis.queries.GQueries;
 import edu.gslis.queries.GQueriesJsonImpl;
 import edu.gslis.searchhits.SearchHitsBatch;
-import edu.gslis.utils.Configuration;
-import edu.gslis.utils.SimpleConfiguration;
 import edu.gslis.utils.Stopper;
+import edu.gslis.utils.config.Configuration;
+import edu.gslis.utils.config.SimpleConfiguration;
 
 public class RM3CrossValidationDemo {
 
