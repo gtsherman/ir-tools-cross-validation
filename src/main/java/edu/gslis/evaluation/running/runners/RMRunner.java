e81f8abe48316e9a88bb5b1944a4a33200f3b970
Garrick Sherman <gsherma2@illinois.edu>
2016-11-17 14:31:02 -0800

    improved RMRunner

--- src/main/java/edu/gslis/evaluation/running/runners/RMRunner.java
+++ src/main/java/edu/gslis/evaluation/running/runners/RMRunner.java
@@ -1,12 +1,12 @@
 package edu.gslis.evaluation.running.runners;
 
-import java.io.File;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
 import edu.gslis.evaluation.evaluators.Evaluator;
 import edu.gslis.evaluation.running.QueryRunner;
+import edu.gslis.evaluation.running.runners.support.ParameterizedResults;
 import edu.gslis.indexes.IndexWrapperIndriImpl;
 import edu.gslis.queries.GQueries;
 import edu.gslis.queries.GQuery;
@@ -15,7 +15,6 @@ import edu.gslis.searchhits.SearchHits;
 import edu.gslis.searchhits.SearchHitsBatch;
 import edu.gslis.textrepresentation.FeatureVector;
 import edu.gslis.utils.Stopper;
-import edu.gslis.utils.readers.RelevanceModelReader;
 
 public class RMRunner implements QueryRunner {
 
@@ -24,19 +23,13 @@ public class RMRunner implements QueryRunner {
 	public static final String FEEDBACK_TERMS = "fbTerms";
 	
 	private IndexWrapperIndriImpl index;
-	private String rmsDir;
 	private Stopper stopper;
 	
-	private Map<GQuery, FeatureVector> queryRMs;
+	private ParameterizedResults processedQueries = new ParameterizedResults();
 	
 	public RMRunner(IndexWrapperIndriImpl index, Stopper stopper) {
-		this(index, stopper, null);
-	}
-	
-	public RMRunner(IndexWrapperIndriImpl index, Stopper stopper, String rmsDir) {
 		this.index = index;
 		this.stopper = stopper;
-		this.rmsDir = rmsDir;
 	}
 	
 	public Map<String, Double> sweep(GQueries queries, Evaluator evaluator) {
@@ -44,6 +37,9 @@ public class RMRunner implements QueryRunner {
 
 		Map<String, Double> bestParams = new HashMap<String, Double>();
 		Map<String, Double> currentParams = new HashMap<String, Double>();
+		
+		currentParams.put(FEEDBACK_DOCUMENTS, 20.0);
+		currentParams.put(FEEDBACK_TERMS, 20.0);
 		
 		for (int origW = 0; origW <= 10; origW++) {
 			double origWeight = origW / 10.0;
@@ -63,41 +59,41 @@ public class RMRunner implements QueryRunner {
 	}
 
 	public SearchHitsBatch run(GQueries queries, int numResults, Map<String, Double> params) {
-		// Read the RMs and store them so we don't have to do it each time this is run,
-		// but implies that fbdocs and fbterms will not change.
-		if (rmsDir != null) {
-			readRMs(queries);
-		}
-
 		SearchHitsBatch batchResults = new SearchHitsBatch();
 		Iterator<GQuery> queryIt = queries.iterator();
 		while (queryIt.hasNext()) {
 			GQuery query = queryIt.next();
+			SearchHits results = getProcessedQuery(query, numResults, params);
+			batchResults.setSearchHits(query.getTitle(), results);
+		}
+		return batchResults;
+	}
+	
+	private SearchHits getProcessedQuery(GQuery query, int numResults, Map<String, Double> params) {
+		double[] paramVals = {params.get(ORIG_QUERY_WEIGHT), params.get(FEEDBACK_DOCUMENTS), params.get(FEEDBACK_TERMS), numResults};
+		
+		if (!processedQueries.resultsExist(query, paramVals)) {
 			query.applyStopper(stopper);
 
-			FeatureVector rmVec;
-			if (rmsDir != null) {
-				rmVec = queryRMs.get(query);
-			} else {
-				FeedbackRelevanceModel rm1 = new FeedbackRelevanceModel();
-				try {
-					rm1.setDocCount(params.get(FEEDBACK_DOCUMENTS).intValue());
-				} catch (NullPointerException e) {
-					rm1.setDocCount(20);
-				}
-				try {
-					rm1.setTermCount(params.get(FEEDBACK_TERMS).intValue());
-				} catch (NullPointerException e) {
-					rm1.setTermCount(20);
-				}
-				rm1.setIndex(index);
-				rm1.setStopper(stopper);
-				rm1.setOriginalQuery(query);
-				
-				rm1.build();
-				rmVec = rm1.asGquery().getFeatureVector();
-				rmVec.normalize();
+			int fbDocs = 20;
+			if (params.containsKey(FEEDBACK_DOCUMENTS)) {
+				fbDocs = params.get(FEEDBACK_DOCUMENTS).intValue();
 			}
+			int fbTerms = 20;
+			if (params.containsKey(FEEDBACK_TERMS)) {
+				fbTerms = params.get(FEEDBACK_TERMS).intValue();
+			}
+
+			FeedbackRelevanceModel rm1 = new FeedbackRelevanceModel();
+			rm1.setDocCount(fbDocs);
+			rm1.setTermCount(fbTerms);
+			rm1.setIndex(index);
+			rm1.setStopper(stopper);
+			rm1.setOriginalQuery(query);
+			rm1.build();
+
+			FeatureVector rmVec = rm1.asGquery().getFeatureVector();
+			rmVec.normalize();
 
 			FeatureVector rm3 = FeatureVector.interpolate(query.getFeatureVector(), rmVec, params.get(ORIG_QUERY_WEIGHT));
 			
@@ -106,24 +102,10 @@ public class RMRunner implements QueryRunner {
 			newQuery.setFeatureVector(rm3);
 			
 			SearchHits results = index.runQuery(newQuery, numResults);
-			batchResults.setSearchHits(query.getTitle(), results);
+			processedQueries.addResults(results, query, paramVals);
 		}
-		return batchResults;
-	}
 
-	private void readRMs(GQueries queries) {
-		Map<GQuery, FeatureVector> queryRMs = new HashMap<GQuery, FeatureVector>();
-		
-		Iterator<GQuery> queryIt = queries.iterator();
-		while (queryIt.hasNext()) {
-			GQuery query = queryIt.next();
-			if (!queryRMs.containsKey(query)) {
-				RelevanceModelReader reader = new RelevanceModelReader(new File(rmsDir+File.pathSeparator+query.getTitle()));
-				queryRMs.put(query, reader.getFeatureVector());
-			}
-		}
-		
-		this.queryRMs = queryRMs;
+		return processedQueries.getResults(query, paramVals);
 	}
 
 }
